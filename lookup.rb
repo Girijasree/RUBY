312eec4b2b26b8cfb3e141f549b5a89958c202c6
Girijasree <girijasree1234gmail.com>
2020-01-14 23:45:04 +0530

    second submission

--- lookup.rb
+++ lookup.rb
@@ -12,36 +12,42 @@ domain = get_command_line_argument
 dns_raw = File.readlines("zone.txt")
 
 def parse_dns(dns_raw)
-  dns_records = { :A => {}, :CNAME => {} }
-  dns_raw.each do |value|
-    record_set = value.split(",")
-    if (record_set[0] == "A")
-      dns_records[:A][record_set[1]] = record_set[2].strip
+  #creating a hashe of hashes
+
+  dns_records = { :a => {}, :cname => {} }
+  dns_raw.each do |record|
+    record = record.strip.split(", ")
+    if (record[0] == "A")
+      #inserting record into the hash containg class "A" records
+      dns_records[:a][record[1]] = record[2].rstrip
     else
-      dns_records[:CNAME][record_set[1]] = record_set[2].strip
+      #inserting record into the hash containg class "CNAME" records
+      dns_records[:cname][record[1]] = record[2].rstrip
     end
   end
   dns_records
 end
 
 def resolve(dns_records, lookup_chain, domain)
-  records_Set1 = dns_records[:A]
-  records_Set2 = dns_records[:CNAME]
-if records_Set1.has_key?(domain)
-    lookup_chain.push(records_Set1[domain])
-    lookup_chain
-elsif records_Set2.has_key?(domain)
-
-  lookup_chain.push(records_Set2[domain])
-  lookup_chain = resolve(dns_records, lookup_chain, records_Set2[domain])
-else
-    puts "Error: record not found for #{domain}"
-    lookup_chain
+  record_hash = dns_records[:a]
+  #first checking, if the given domain present is a class A type record
+  if record_hash.has_key?(domain)
+    lookup_chain.push(record_hash[domain])
+    return lookup_chain
+  else
+    record_hash = dns_records[:cname]
+    #checking in the CNAME record
+    if record_hash.has_key?(domain)
+      lookup_chain.push(record_hash[domain])
+      lookup_chain = resolve(dns_records, lookup_chain, record_hash[domain])
+    else
+      puts "Error: record not found for #{domain}"
+      lookup_chain
+    end
   end
 end
 
 dns_records = parse_dns(dns_raw)
-
 lookup_chain = [domain]
 lookup_chain = resolve(dns_records, lookup_chain, domain)
-puts lookup_chain.join(" => ")
+puts lookup_chain.join(" => ") if lookup_chain.length() > 1
